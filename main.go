0b5bd1107105759d701e3fa21c9c48d5473cd71f
zainfikrizain <zainfikrizain@gmail.com>
2026-02-01 22:03:18 +0700

    a

--- main.go
+++ main.go
@@ -4,12 +4,29 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strings"
+
+	"github.com/spf13/viper"
 )
 
 func main() {
-	config := LoadConfig()
+	type Config struct {
+		Port string `mapstructure:"PORT"`
+	}
 
-	// Default port fallback
+	if _, err := os.Stat(".env"); err == nil {
+		viper.SetConfigFile(".env")
+		_ = viper.ReadInConfig()
+	}
+
+	viper.AutomaticEnv()
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+
+	config := Config{
+		Port: viper.GetString("PORT"),
+	}
+
 	if config.Port == "" {
 		config.Port = "8080"
 	}
@@ -17,8 +34,5 @@ func main() {
 	addr := "0.0.0.0:" + config.Port
 	fmt.Println("Server running di", addr)
 
-	err := http.ListenAndServe(addr, nil)
-	if err != nil {
-		log.Fatal("gagal running server:", err)
-	}
+	log.Fatal(http.ListenAndServe(addr, nil))
 }
